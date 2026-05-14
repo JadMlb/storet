62476cf4e3a61fe0f6d84771f08d37ca2c50e7fc
JadMlb <jad_mlb@outlook.com>
2026-05-14 21:49:41 +0200

    [FIX-FRONTEND]: made suspense with delay to avoid flickering

--- frontend/src/app/shared/components/suspense/suspense.ts
+++ frontend/src/app/shared/components/suspense/suspense.ts
@@ -1,5 +1,7 @@
-import { booleanAttribute, Component, Input } from '@angular/core';
+import { booleanAttribute, Component, DestroyRef, inject, input, Input, signal } from '@angular/core';
 import { Skeleton } from '../skeleton/skeleton';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { map, of, switchMap, timer } from 'rxjs';
 
 @Component ({
   selector: 'suspense',
@@ -9,6 +11,29 @@ import { Skeleton } from '../skeleton/skeleton';
 })
 export class Suspense
 {
-  @Input ({transform: booleanAttribute})
-  loading: boolean = false;
+  loading = input.required<boolean>();
+
+  protected showSkeleton = signal (false);
+
+  private destroyRef = inject (DestroyRef);
+
+  constructor ()
+  {
+    const loading$ = toObservable (this.loading);
+    loading$.pipe (
+      switchMap (
+        isLoading =>
+        {
+          if (!isLoading)
+            return of (false);
+          return timer (200)
+                  .pipe (map (() => true));
+        }
+      ),
+      takeUntilDestroyed (this.destroyRef)
+    )
+    .subscribe (
+      show => this.showSkeleton.set (show)
+    );
+  }
 }
